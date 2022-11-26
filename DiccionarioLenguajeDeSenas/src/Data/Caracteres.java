36a5aeebacff9cecbe99b3289453b3ba057248aa
Nicolas David Orjuela Sanchez <norjuelas@unal.edu.co>
2022-11-26 13:21:56 -0500

    Proyecto Diccionario lenguaje de señas
    Comienzo de la primera etapa del proyecto, construccion de la capa Data.

---
+++ DiccionarioLenguajeDeSenas/src/Data/Caracteres.java
@@ -0,0 +1,22 @@
+
+package Data;
+
+/**
+ *
+ * @author Nicolas Orjuela
+ */
+
+
+/**esta sera la clase abstracta con nivel superior 
+* de herenciacon respecto a letras, numeros y signos especiales
+* que seran otras clases hijas de esta clase.
+* 
+* La idea es que cree esas clases e instancie el abecedario, mientras
+* yo hago lo mismo con las palabras basicas.
+* 
+* el siguiente paso sera construir la intergas grafica que permita imprimir 
+* el atributo imagen de cada palabra cuando esta sea solicitada al programa
+*/
+public class Caracteres {
+    
+}
---
+++ DiccionarioLenguajeDeSenas/src/diccionariolenguajedesenas/DiccionarioLenguajeDeSenas.java
@@ -0,0 +1,15 @@
+
+package diccionariolenguajedesenas;
+
+/**
+ *
+ * @author Nicolas Orjuela y Jeison Diaz
+ */
+
+public class DiccionarioLenguajeDeSenas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
